5c11ff326c17ea7ea631a99a665b96e7c770ea60
sofush <102423577+sofush@users.noreply.github.com>
2025-03-13 15:47:04 +0100

    feat: add button release events and serialize to json

--- embedded/src/buttons_setup.rs
+++ embedded/src/buttons_setup.rs
@@ -8,35 +8,40 @@ use microbit::{
     board::Buttons,
     pac::{self, GPIOTE},
 };
+use serde::Serialize;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum Button {
-    Left,
-    Right,
-    Both,
+#[derive(Clone, Copy, PartialEq, Eq, Serialize, Default, Debug)]
+pub struct ButtonState {
+    left_pressed: bool,
+    right_pressed: bool,
 }
 
 pub static GPIO: Mutex<RefCell<Option<Gpiote>>> =
     Mutex::new(RefCell::new(None));
-pub static BUTTON_STATE: Mutex<RefCell<Option<Button>>> =
+pub static BUTTON_STATE: Mutex<RefCell<Option<ButtonState>>> =
     Mutex::new(RefCell::new(None));
 
 pub(crate) fn init_buttons(board_gpiote: GPIOTE, board_buttons: Buttons) {
     let gpiote = Gpiote::new(board_gpiote);
 
+    let a_button = board_buttons.button_a.degrade();
+    let b_button = board_buttons.button_b.degrade();
+
     let channel0 = gpiote.channel0();
-    channel0
-        .input_pin(&board_buttons.button_a.degrade())
-        .hi_to_lo()
-        .enable_interrupt();
+    channel0.input_pin(&a_button).hi_to_lo().enable_interrupt();
     channel0.reset_events();
 
     let channel1 = gpiote.channel1();
-    channel1
-        .input_pin(&board_buttons.button_b.degrade())
-        .hi_to_lo()
-        .enable_interrupt();
+    channel1.input_pin(&a_button).lo_to_hi().enable_interrupt();
     channel1.reset_events();
+
+    let channel2 = gpiote.channel2();
+    channel2.input_pin(&b_button).hi_to_lo().enable_interrupt();
+    channel2.reset_events();
+
+    let channel3 = gpiote.channel3();
+    channel3.input_pin(&b_button).lo_to_hi().enable_interrupt();
+    channel3.reset_events();
 
     free(move |cs| {
         *GPIO.borrow(cs).borrow_mut() = Some(gpiote);
@@ -54,31 +59,37 @@ fn GPIOTE() {
     free(|cs| {
         if let Some(gpiote) = GPIO.borrow(cs).borrow().as_ref() {
             let a_pressed = gpiote.channel0().is_event_triggered();
-            let b_pressed = gpiote.channel1().is_event_triggered();
+            let a_released = gpiote.channel1().is_event_triggered();
+            let b_pressed = gpiote.channel2().is_event_triggered();
+            let b_released = gpiote.channel3().is_event_triggered();
 
-            let new_state = match (a_pressed, b_pressed) {
-                (true, false) => Some(Button::Left),
-                (false, true) => Some(Button::Right),
-                (true, true) => Some(Button::Both),
-                _ => None,
-            };
+            let mut state = BUTTON_STATE.borrow(cs).borrow_mut();
+            let state: &mut ButtonState = state.get_or_insert_default();
+
+            if a_pressed {
+                state.left_pressed = true;
+            }
+
+            if a_released {
+                state.left_pressed = false;
+            }
+
+            if b_pressed {
+                state.right_pressed = true;
+            }
+
+            if b_released {
+                state.right_pressed = false;
+            }
 
             gpiote.channel0().reset_events();
             gpiote.channel1().reset_events();
-
-            *BUTTON_STATE.borrow(cs).borrow_mut() = new_state;
+            gpiote.channel2().reset_events();
+            gpiote.channel3().reset_events();
         }
     });
 }
 
-pub fn get_button_event(reset: bool) -> Option<Button> {
-    free(|cs| {
-        let button = *BUTTON_STATE.borrow(cs).borrow();
-
-        if reset {
-            *BUTTON_STATE.borrow(cs).borrow_mut() = None
-        }
-
-        button
-    })
+pub fn get_button_state() -> Option<ButtonState> {
+    free(|cs| *BUTTON_STATE.borrow(cs).borrow())
 }
--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -2,6 +2,10 @@
 #![no_main]
 #![no_std]
 
+mod buttons_setup;
+mod serial_setup;
+
+use buttons_setup::{ButtonState, init_buttons};
 use cortex_m_rt::entry;
 use embedded_hal_nb::serial::Write;
 use microbit::hal::{
@@ -9,13 +13,8 @@ use microbit::hal::{
     uarte::{Baudrate, Parity},
 };
 use panic_rtt_target as _;
-use rtt_target::rtt_init_print;
-
-mod serial_setup;
+use rtt_target::{rprintln, rtt_init_print};
 use serial_setup::UartePort;
-
-mod buttons_setup;
-use buttons_setup::init_buttons;
 
 #[entry]
 fn main() -> ! {
@@ -35,17 +34,27 @@ fn main() -> ! {
         UartePort::new(uarte)
     };
 
-    loop {
-        let event = buttons_setup::get_button_event(true);
+    let mut latest_state = ButtonState::default();
 
-        let Some(event) = event else {
+    loop {
+        let button_state = buttons_setup::get_button_state();
+
+        let Some(button_state) = button_state else {
             continue;
         };
 
-        let msg = match event {
-            buttons_setup::Button::Left => "Left",
-            buttons_setup::Button::Right => "Right",
-            buttons_setup::Button::Both => "Both",
+        if button_state == latest_state {
+            continue;
+        }
+
+        latest_state = button_state;
+
+        let Ok(msg) = serde_json_core::to_string::<_, 100>(&button_state)
+        else {
+            rprintln!("Error");
+            write!(serial, "Error").unwrap();
+            nb::block!(serial.flush()).unwrap();
+            continue;
         };
 
         use core::fmt::Write;
