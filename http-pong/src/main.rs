c3f958587893f639b26ad99f86f49a50c3db8958
sofush <102423577+sofush@users.noreply.github.com>
2025-03-14 13:49:47 +0100

    feat: implement websocket server endpoint

--- http-pong/src/main.rs
+++ http-pong/src/main.rs
@@ -7,19 +7,21 @@ use actix_web::{
 };
 use actix_ws::AggregatedMessage;
 use env_logger::Env;
-use mblistener::ButtonState;
+use futures_util::FutureExt;
+use futures_util::StreamExt;
+use futures_util::pin_mut;
 use tokio::sync::broadcast::Sender;
 use tokio::sync::broadcast::channel;
 
 mod mblistener;
 
 struct AppData {
-    sender: Sender<ButtonState>,
+    microbit_sender: Sender<String>,
 }
 
 enum SelectResult {
     WebsocketMessage(AggregatedMessage),
-    BroadcastMessage(ButtonState),
+    MicrobitMessage(String),
     Error(anyhow::Error),
 }
 
@@ -28,7 +30,57 @@ async fn websocket(
     stream: web::Payload,
     data: web::Data<AppData>,
 ) -> Result<HttpResponse, Error> {
-    todo!()
+    let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
+
+    let mut stream = stream
+        .aggregate_continuations()
+        .max_continuation_size(2_usize.pow(20))
+        .fuse();
+
+    let mut receiver = data.microbit_sender.subscribe();
+
+    rt::spawn(async move {
+        loop {
+            let next_ws_message = stream.select_next_some();
+            let next_broadcast = receiver.recv().fuse();
+
+            pin_mut!(next_broadcast, next_ws_message);
+
+            let message = futures_util::select! {
+                ret = next_ws_message => {
+                    match ret {
+                        Ok(r) => SelectResult::WebsocketMessage(r),
+                        Err(e) => SelectResult::Error(e.into()),
+                    }
+                },
+                ret = next_broadcast => {
+                    match ret {
+                        Ok(r) => SelectResult::MicrobitMessage(r),
+                        Err(e) => SelectResult::Error(e.into()),
+                    }
+                },
+            };
+
+            match message {
+                SelectResult::WebsocketMessage(msg) => {
+                    if let AggregatedMessage::Close(close_reason) = msg {
+                        println!("Websocket disconnected: {close_reason:?}");
+                    }
+                }
+                SelectResult::MicrobitMessage(msg) => {
+                    if session.text(msg).await.is_err() {
+                        // session closed
+                        break;
+                    }
+                }
+                SelectResult::Error(error) => {
+                    println!("Error: {error}");
+                }
+            }
+        }
+    });
+
+    Ok(res)
 }
 
 async fn index() -> impl Responder {
@@ -37,7 +89,7 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let (tx, _rx) = channel::<ButtonState>(1_000_000);
+    let (tx, _rx) = channel::<String>(1_000_000);
     let tx_clone = tx.clone();
 
     env_logger::init_from_env(Env::new());
@@ -52,7 +104,9 @@ async fn main() -> std::io::Result<()> {
         let tx_clone = tx.clone();
 
         App::new()
-            .app_data(web::Data::new(AppData { sender: tx_clone }))
+            .app_data(web::Data::new(AppData {
+                microbit_sender: tx_clone,
+            }))
             .service(web::resource("/").to(index))
             .service(Files::new("/public", "./public").prefer_utf8(true))
             .service(
